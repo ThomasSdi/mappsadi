016478d8e05dc37855001094bba3351aeb8eeefd
ThomasSdi <sadithomas7@gmail.com>
2024-05-03 01:27:10 +0200

    petite modif

--- src/pages/api/adresses/[adresseID].js
+++ src/pages/api/adresses/[adresseID].js
@@ -1,24 +1,24 @@
 /* eslint-disable eol-last, quotes */
-import axios from "axios";
-import { createRoute } from "@/api/createRoute";
-import { AdressesModel } from "@/database/models/AdressesModel";
+import axios from "axios"
+import { createRoute } from "@/api/createRoute"
+import { AdressesModel } from "@/database/models/AdressesModel"
 
 const handler = createRoute(async (req, res) => {
-  const { adresseId } = req.query;
-  const adresse = await AdressesModel.findById(adresseId);
+  const { adresseId } = req.query
+  const adresse = await AdressesModel.findById(adresseId)
 
   if (!adresse) {
-    res.status(404).send({ error: "non trouvé" });
-    return;
+    res.status(404).send({ error: "non trouvé" })
+    return
   }
 
   if (req.method === "GET") {
-    res.send(adresse);
-    return;
+    res.send(adresse)
+    return
   }
 
   if (req.method === "PUT") {
-    const { type, nom, adresse, ville, codePostal, pays } = req.body;
+    const { type, nom, adresse, ville, codePostal, pays } = req.body
 
     Object.assign(adresse, {
       type,
@@ -27,19 +27,19 @@ const handler = createRoute(async (req, res) => {
       ville,
       codePostal,
       pays,
-    });
+    })
 
-    await adresse.save();
+    await adresse.save()
 
-    res.send(adresse);
-    return;
+    res.send(adresse)
+    return
   }
 
   if (req.method === "DELETE") {
-    await adresse.deleteOne();
-    res.send(adresse);
-    return;
+    await adresse.deleteOne()
+    res.send(adresse)
+    return
   }
-});
+})
 
-export default handler;
+export default handler
